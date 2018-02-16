3605f9c0657f5a39a7ab757386716541728b7cd3
Jan Brouwer <jan@brewsky.nl>
2018-02-16 23:17:07 +0100

    Export BaseQuantities as IfcElementQuantity

--- src/bt_ifcmanager/lib/lib_ifc/IfcRelDefinesByProperties_su.rb
+++ src/bt_ifcmanager/lib/lib_ifc/IfcRelDefinesByProperties_su.rb
@@ -26,6 +26,10 @@ require_relative 'IfcText.rb'
 require_relative 'IfcBoolean.rb'
 require_relative File.join('IFC2X3', 'IfcPropertySet.rb')
 require_relative File.join('IFC2X3', 'IfcPropertySingleValue.rb')
+require_relative File.join('IFC2X3', 'IfcElementQuantity.rb')
+require_relative File.join('IFC2X3', 'IfcQuantityArea.rb')
+require_relative File.join('IFC2X3', 'IfcQuantityVolume.rb')
+require_relative File.join('IFC2X3', 'IfcQuantityLength.rb')
 
 module BimTools
   module IfcRelDefinesByProperties_su
@@ -38,37 +42,67 @@ module BimTools
       @relatedobjects = IfcManager::Ifc_Set.new()
       if sketchup.is_a?( Sketchup::AttributeDictionary )
         attr_dict = sketchup
-        pset = BimTools::IFC2X3::IfcPropertySet.new( ifc_model, attr_dict )
-        @relatingpropertydefinition = pset
-        pset.name = BimTools::IfcManager::IfcLabel.new( attr_dict.name ) unless attr_dict.name.nil?
-        pset.hasproperties = IfcManager::Ifc_Set.new()
-        if attr_dict.length == 0 && attr_dict.attribute_dictionaries
+        if attr_dict.name == "BaseQuantities" # export as elementquantities
+          
+          qty = BimTools::IFC2X3::IfcElementQuantity.new( ifc_model, attr_dict )
+          @relatingpropertydefinition = qty
+          qty.name = BimTools::IfcManager::IfcLabel.new( attr_dict.name ) unless attr_dict.name.nil?
+          qty.quantities = IfcManager::Ifc_Set.new()
           attr_dict.attribute_dictionaries.each { | dict |
-            prop = BimTools::IFC2X3::IfcPropertySingleValue.new( ifc_model, attr_dict )
-            prop.name = BimTools::IfcManager::IfcIdentifier.new( dict.name )
-            
-            # get value type
-            case dict['attribute_type']
-            when "double"
-              prop.nominalvalue = BimTools::IfcManager::IfcReal.new( dict['value'] )
-            when "boolean"
-              prop.nominalvalue = BimTools::IfcManager::IfcBoolean.new( dict['value'] )
-            #when "string"
-            #  prop.nominalvalue = BimTools::IfcManager::IfcLabel.new( dict['value'] ) # (!) not always IfcLabel
-            else
-              prop.nominalvalue = BimTools::IfcManager::IfcLabel.new( dict['value'] ) # (!) not always IfcLabel
+            case dict.name
+            when "Area", "GrossArea"
+              prop = BimTools::IFC2X3::IfcQuantityArea.new( ifc_model, attr_dict )
+              prop.name = BimTools::IfcManager::IfcIdentifier.new( dict.name )
+              prop.areavalue = BimTools::IfcManager::IfcReal.new( dict['value'] ) # real should be IfcLengthMeasure
+              qty.quantities.add( prop )
+            when "Volume"
+              prop = BimTools::IFC2X3::IfcQuantityVolume.new( ifc_model, attr_dict )
+              prop.name = BimTools::IfcManager::IfcIdentifier.new( dict.name )
+              prop.volumevalue = BimTools::IfcManager::IfcReal.new( dict['value'] ) # real should be IfcLengthMeasure
+              qty.quantities.add( prop )
+            when "Width", "Height", "Depth", "Perimeter"
+              prop = BimTools::IFC2X3::IfcQuantityLength.new( ifc_model, attr_dict )
+              prop.name = BimTools::IfcManager::IfcIdentifier.new( dict.name )
+              prop.lengthvalue = BimTools::IfcManager::IfcReal.new( dict['value'] ) # real should be IfcLengthMeasure
+              qty.quantities.add( prop )
+            #else
             end
-            prop.nominalvalue.long = true # adding long = true returns a full object string, necessary for propertyset
-            pset.hasproperties.add( prop )
           }
-        else
-          attr_dict.each { | key, value |
-            prop = BimTools::IFC2X3::IfcPropertySingleValue.new( ifc_model, attr_dict )
-            prop.name = BimTools::IfcManager::IfcIdentifier.new( key )
-            prop.nominalvalue = BimTools::IfcManager::IfcLabel.new( value ) # (!) not always IfcLabel
-            prop.nominalvalue.long = true # adding long = true returns a full object string
-            pset.hasproperties.add( prop )
-          }
+          
+        else # export as propertyset
+          pset = BimTools::IFC2X3::IfcPropertySet.new( ifc_model, attr_dict )
+          @relatingpropertydefinition = pset
+          pset.name = BimTools::IfcManager::IfcLabel.new( attr_dict.name ) unless attr_dict.name.nil?
+          pset.hasproperties = IfcManager::Ifc_Set.new()
+          if attr_dict.length == 0 && attr_dict.attribute_dictionaries
+            attr_dict.attribute_dictionaries.each { | dict |
+              
+              prop = BimTools::IFC2X3::IfcPropertySingleValue.new( ifc_model, attr_dict )
+              prop.name = BimTools::IfcManager::IfcIdentifier.new( dict.name )
+              
+              # get value type
+              case dict['attribute_type']
+              when "double"
+                prop.nominalvalue = BimTools::IfcManager::IfcReal.new( dict['value'] )
+              when "boolean"
+                prop.nominalvalue = BimTools::IfcManager::IfcBoolean.new( dict['value'] )
+              #when "string"
+              #  prop.nominalvalue = BimTools::IfcManager::IfcLabel.new( dict['value'] ) # (!) not always IfcLabel
+              else
+                prop.nominalvalue = BimTools::IfcManager::IfcLabel.new( dict['value'] ) # (!) not always IfcLabel
+              end
+              prop.nominalvalue.long = true # adding long = true returns a full object string, necessary for propertyset
+              pset.hasproperties.add( prop )
+            }
+          else
+            attr_dict.each { | key, value |
+              prop = BimTools::IFC2X3::IfcPropertySingleValue.new( ifc_model, attr_dict )
+              prop.name = BimTools::IfcManager::IfcIdentifier.new( key )
+              prop.nominalvalue = BimTools::IfcManager::IfcLabel.new( value ) # (!) not always IfcLabel
+              prop.nominalvalue.long = true # adding long = true returns a full object string
+              pset.hasproperties.add( prop )
+            }
+          end
         end
       end
     end # def sketchup
