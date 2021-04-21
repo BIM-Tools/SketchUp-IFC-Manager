899621b05ac7ec5ad11008c3d605838a91e4b2d8
Jan Brouwer <jan@brewsky.nl>
2021-04-21 23:38:39 +0200

    Release version 3.5.0

--- src/bt_ifcmanager.rb
+++ src/bt_ifcmanager.rb
@@ -32,7 +32,7 @@ module BimTools
 
   module IfcManager
     # Version and release information.
-    VERSION = '3.4.7'
+    VERSION = '3.5.0'
 
     # load plugin only if SketchUp version is PRO
     # raised minimum version to 2017 due to switch to htmldialog
