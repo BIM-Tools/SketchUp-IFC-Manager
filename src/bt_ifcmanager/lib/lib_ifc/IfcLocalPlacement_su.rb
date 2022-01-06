34c08dcd0f457f24ad90226b283608239160e4f3
Jan Brouwer <jan@brewsky.nl>
2022-01-06 16:58:04 +0100

    Allow creation of empty IfcLocalPlacement

--- src/bt_ifcmanager/lib/lib_ifc/IfcLocalPlacement_su.rb
+++ src/bt_ifcmanager/lib/lib_ifc/IfcLocalPlacement_su.rb
@@ -24,47 +24,41 @@ module BimTools
     include BimTools::IfcManager::Settings.ifc_module
 
     attr_accessor :transformation, :ifc_total_transformation
-    def initialize(ifc_model, su_total_transformation, placementrelto=nil )
+
+    def initialize(ifc_model, su_total_transformation = nil, placementrelto = nil)
       super
-      @ifc_model = ifc_model
-      
+
       # set parent placement
-      if placementrelto.is_a? IfcLocalPlacement
-        @placementrelto = placementrelto
+      @placementrelto = placementrelto if placementrelto.is_a? IfcLocalPlacement
+
+      if su_total_transformation.is_a? Geom::Transformation
+
+        # fix y-axis direction if flipped
+        t = su_total_transformation
+        axis_fix = if t.xaxis.cross(t.yaxis).samedirection?(t.zaxis)
+                     Geom::Transformation.axes([0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1])
+                   else
+                     Geom::Transformation.axes([0, 0, 0], [1, 0, 0], [0, -1, 0], [0, 0, 1])
+                   end
+
+        # strip out scaling
+        t = su_total_transformation.to_a
+        x = t[0..2].normalize # is the xaxis
+        y = t[4..6].normalize # is the yaxis
+        z = t[8..10].normalize # is the zaxis
+        no_scale = Geom::Transformation.axes(su_total_transformation.origin, x, y, z)
+
+        @ifc_total_transformation = no_scale * axis_fix
+
+        @transformation = if @placementrelto.nil? && @placementrelto.ifc_total_transformation
+                            @placementrelto.ifc_total_transformation.inverse * @ifc_total_transformation
+                          else
+                            @ifc_total_transformation
+                          end
+
+        # set relativeplacement
+        @relativeplacement = IfcAxis2Placement3D.new(ifc_model, @transformation)
       end
-      
-      # fix y-axis direction if flipped
-      t = su_total_transformation
-      if t.xaxis.cross(t.yaxis).samedirection?( t.zaxis )
-        axis_fix = Geom::Transformation.axes( [0,0,0], [1,0,0], [0,1,0], [0,0,1] )
-      else
-        axis_fix = Geom::Transformation.axes( [0,0,0], [1,0,0], [0,-1,0], [0,0,1] )
-      end
-      
-      # strip out scaling
-      t = su_total_transformation.to_a
-      #sx = Geom::Vector3d.new([t[0],t[4],t[8]])
-      #sy = Geom::Vector3d.new([t[1],t[5],t[9]])
-      #sz = Geom::Vector3d.new([t[2],t[6],t[10]])
-      #scale = Geom::Transformation.scaling(sx.length, sy.length, sz.length)
-      x = t[0..2].normalize # is the xaxis
-      y = t[4..6].normalize # is the yaxis
-      z = t[8..10].normalize # is the zaxis
-      no_scale = Geom::Transformation.axes(su_total_transformation.origin, x, y, z)
-      
-      @ifc_total_transformation = no_scale * axis_fix
-      
-      if @placementrelto
-        @transformation = @placementrelto.ifc_total_transformation.inverse * @ifc_total_transformation
-      else
-        @transformation = @ifc_total_transformation
-      end
-      
-      # set relativeplacement
-      @relativeplacement = IfcAxis2Placement3D.new( @ifc_model, @transformation )
-      @relativeplacement.location = IfcCartesianPoint.new( @ifc_model, @transformation.origin )
-      @relativeplacement.axis = IfcDirection.new( @ifc_model, @transformation.zaxis )
-      @relativeplacement.refdirection = IfcDirection.new( @ifc_model, @transformation.xaxis )
     end
-  end # module IfcLocalPlacement_su
-end # module BimTools
+  end
+end
