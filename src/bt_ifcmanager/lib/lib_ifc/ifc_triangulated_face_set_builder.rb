21ba741778b56741c5395c90e61d2913b26343b8
Jan Brouwer <jbrouwer@digibase.nl>
2024-04-01 12:24:44 +0200

    Fix IfcTriangulatedFaceSetBuilder missing geometry

--- src/bt_ifcmanager/lib/lib_ifc/ifc_triangulated_face_set_builder.rb
+++ src/bt_ifcmanager/lib/lib_ifc/ifc_triangulated_face_set_builder.rb
@@ -32,6 +32,10 @@ module BimTools
       def self.build(ifc_model)
         builder = new(ifc_model)
         yield(builder)
+
+        builder.ifc_triangulated_face_set.coordinates ||= IfcManager::Types::List.new
+        builder.ifc_triangulated_face_set.coordindex ||= IfcManager::Types::List.new
+
         builder.ifc_triangulated_face_set
       end
 
@@ -40,6 +44,10 @@ module BimTools
         @ifc_model = ifc_model
         @ifc_triangulated_face_set = @ifc::IfcTriangulatedFaceSet.new(ifc_model)
         @ifc_triangulated_face_set.coordinates = @ifc::IfcCartesianPointList3D.new(ifc_model)
+
+        # # @todo closed should be true for manifold geometry
+        # @closed = false
+        @coordindex = IfcManager::Types::List.new
       end
 
       def set_faces(
@@ -56,82 +64,29 @@ module BimTools
         uv_coordinates_back = []
         normals_front = []
         normals_back = []
-        polygons = []
         point_total = 0
-        face_mesh_id = 0
-        add_parent_texture = false
 
-        meshes = faces.map do |face|
-          add_parent_texture = true if face.material.nil? && !add_parent_texture
-          [face.mesh(7), get_face_transformation(face).inverse]
-        end
+        add_parent_texture = faces.any? { |face| face.material.nil? }
+        add_material_to_model(parent_material) if add_parent_texture
 
-        if add_parent_texture && !ifc_model.materials.key?(parent_material)
-          ifc_model.materials[parent_material] = IfcManager::MaterialAndStyling.new(ifc_model, parent_material)
-        end
+        meshes = faces.map { |face| [face.mesh(7), get_face_transformation(face).inverse] }
 
-        # @todo closed should be true for manifold geometry
-        @closed = false
-        @coordindex = IfcManager::Types::List.new # polygons as indexes
-        while face_mesh_id < meshes.length
-          face_mesh = meshes[face_mesh_id][0]
-          front_texture = if front_material && front_material.texture
-                            true
-                          else
-                            false
-                          end
-          back_texture = if double_sided_faces && back_material && back_material.texture
-                           true
-                         else
-                           false
-                         end
-          parent_texture = if parent_material && parent_material.texture
-                             true
-                           else
-                             false
-                           end
+        meshes.each do |mesh|
+          face_mesh, face_transformation = mesh
+          front_texture = texture_exists?(front_material)
+          back_texture = texture_exists?(back_material) if double_sided_faces
+          parent_texture = texture_exists?(parent_material)
 
+          # Calculate UV transformation for parent texture if no texture is applied to the face
           if !(front_texture || back_texture) && parent_texture
-            face_transformation = meshes[face_mesh_id][1]
-            texture = parent_material.texture
-            texture_transformation = get_texture_transformation(texture)
-            # uv_transformation = transformation * face_transformation
-            uv_transformation = transformation.inverse * face_transformation * texture_transformation
+            uv_transformation = calculate_uv_transformation(transformation, face_transformation,
+                                                            parent_material.texture)
           end
 
           face_mesh.transform! transformation
-          mesh_point_id = 1
-          while mesh_point_id <= face_mesh.count_points
-            index = mesh_point_id + point_total - 1
-            points[index] = face_mesh.point_at(mesh_point_id)
-            if front_material
-              if front_texture
-                uv_coordinates_front[index] = get_uv(face_mesh.uv_at(mesh_point_id, true))
-                normals_front[index] = face_mesh.normal_at(mesh_point_id)
-              end
-            elsif parent_texture
-              uv_coordinates_front[index] = get_uv_global(points[index], uv_transformation)
-              normals_front[index] = face_mesh.normal_at(mesh_point_id)
-            end
-            if double_sided_faces
-              if back_material
-                if back_texture
-                  uv_coordinates_back[index] = get_uv(face_mesh.uv_at(mesh_point_id, false))
-                  normals_back[index] = face_mesh.normal_at(mesh_point_id)
-                end
-              elsif parent_texture
-                uv_coordinates_back[index] = get_uv_global(points[index], uv_transformation)
-                normals_back[index] = face_mesh.normal_at(mesh_point_id)
-              end
-            end
-            mesh_point_id += 1
-          end
 
-          face_mesh.polygons.each do |polygon|
-            @coordindex.add(IfcManager::Types::List.new(get_ifc_polygon(point_total, polygon)))
-          end
-          face_mesh_id += 1
-          point_total += face_mesh.count_points
+          point_total = process_faces(face_mesh, point_total, points, uv_coordinates_front, uv_coordinates_back,
+                                      normals_front, normals_back, front_texture, back_texture, parent_texture, uv_transformation, double_sided_faces)
         end
 
         @ifc_triangulated_face_set.coordinates.coordlist = IfcManager::Types::List.new(points.map do |point|
@@ -140,21 +95,21 @@ module BimTools
           end)
         end)
 
-        return unless ifc_model.textures
+        @ifc_triangulated_face_set.closed = @closed
+        @ifc_triangulated_face_set.normals = IfcManager::Types::List.new(normals_front.map do |normal|
+          IfcManager::Types::List.new(normal.to_a.map do |coord|
+            IfcManager::Types::IfcParameterValue.new(ifc_model, coord)
+          end)
+        end)
+        @ifc_triangulated_face_set.coordindex = @coordindex
 
-        if front_material
-          create_texture_map(ifc_model, front_material, uv_coordinates_front) if front_material.texture
-        elsif parent_material && parent_material.texture
-          create_texture_map(ifc_model, parent_material, uv_coordinates_front)
-        end
-        return unless double_sided_faces
+        return unless ifc_model.textures && (front_texture || back_texture || parent_texture)
 
-        if back_material
-          create_texture_map(ifc_model, back_material, uv_coordinates_back) if back_material.texture
-        elsif parent_material && parent_material.texture
-          create_texture_map(ifc_model, parent_material, uv_coordinates_back)
-        end
+        process_texture_maps(ifc_model, front_material, back_material, parent_material, uv_coordinates_front,
+                             uv_coordinates_back, double_sided_faces)
       end
+
+      private
 
       def create_texture_map(ifc_model, su_material, uv_coordinates)
         return unless su_material
@@ -204,6 +159,75 @@ module BimTools
         plane = [plane_point, normal]
         projected_origin = ORIGIN.project_to_plane(plane)
         Geom::Transformation.axes(projected_origin, axes[0], axes[1], axes[2])
+      end
+
+      def process_faces(face_mesh, point_total, points, uv_coordinates_front, uv_coordinates_back, normals_front,
+                        normals_back, front_texture, back_texture, parent_texture, uv_transformation, double_sided_faces)
+        (1..face_mesh.count_points).each do |mesh_point_id|
+          index = mesh_point_id + point_total - 1
+          points[index] = face_mesh.point_at(mesh_point_id)
+          normals_front[index] = face_mesh.normal_at(mesh_point_id)
+          normals_back[index] = face_mesh.normal_at(mesh_point_id) if double_sided_faces
+          if front_texture || back_texture || parent_texture
+            process_textures(front_texture, back_texture, parent_texture, face_mesh, mesh_point_id, points,
+                             uv_coordinates_front, uv_coordinates_back, uv_transformation, double_sided_faces, index)
+          end
+        end
+
+        face_mesh.polygons.each do |polygon|
+          @coordindex.add(IfcManager::Types::List.new(get_ifc_polygon(point_total, polygon)))
+        end
+
+        point_total += face_mesh.count_points
+        point_total
+      end
+
+      def process_textures(front_texture, back_texture, parent_texture, face_mesh, mesh_point_id, points,
+                           uv_coordinates_front, uv_coordinates_back, uv_transformation, double_sided_faces, index)
+        if front_texture
+          uv_coordinates_front[index] = get_uv(face_mesh.uv_at(mesh_point_id, true))
+        elsif parent_texture
+          uv_coordinates_front[index] = get_uv_global(points[index], uv_transformation)
+        end
+
+        return unless double_sided_faces
+
+        if back_texture
+          uv_coordinates_back[index] = get_uv(face_mesh.uv_at(mesh_point_id, false))
+        elsif parent_texture
+          uv_coordinates_back[index] = get_uv_global(points[index], uv_transformation)
+        end
+      end
+
+      def process_texture_maps(ifc_model, front_material, back_material, parent_material, uv_coordinates_front,
+                               uv_coordinates_back, double_sided_faces)
+        if front_material
+          create_texture_map(ifc_model, front_material, uv_coordinates_front) if front_material.texture
+        elsif parent_material && parent_material.texture
+          create_texture_map(ifc_model, parent_material, uv_coordinates_front)
+        end
+        return unless double_sided_faces
+
+        if back_material
+          create_texture_map(ifc_model, back_material, uv_coordinates_back) if back_material.texture
+        elsif parent_material && parent_material.texture
+          create_texture_map(ifc_model, parent_material, uv_coordinates_back)
+        end
+      end
+
+      def add_material_to_model(parent_material)
+        return if @ifc_model.materials.key?(parent_material)
+
+        @ifc_model.materials[parent_material] = IfcManager::MaterialAndStyling.new(@ifc_model, parent_material)
+      end
+
+      def texture_exists?(material)
+        material && material.texture ? true : false
+      end
+
+      def calculate_uv_transformation(transformation, face_transformation, texture)
+        texture_transformation = get_texture_transformation(texture)
+        transformation.inverse * face_transformation * texture_transformation
       end
     end
   end
