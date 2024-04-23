86374809370509f2df886b2cf65dcd9c6297ea3f
Jan Brouwer <jbrouwer@digibase.nl>
2024-04-23 19:48:45 +0200

    Refactor IfcProduct_su

--- src/bt_ifcmanager/lib/lib_ifc/IfcProduct_su.rb
+++ src/bt_ifcmanager/lib/lib_ifc/IfcProduct_su.rb
@@ -38,71 +38,64 @@ module BimTools
     def initialize(ifc_model, sketchup)
       super
       @ifc = BimTools::IfcManager::Settings.ifc_module
-      if sketchup.respond_to?(:definition)
-        ifc_version = BimTools::IfcManager::Settings.ifc_version
-        @ifc_model = ifc_model
-        @su_object = sketchup
-        definition = @su_object.definition
+      return unless sketchup.respond_to?(:definition)
 
-        # When instance name is set, use that, otherwise use definition name
-        # (?) set name, here? is this a duplicate?
-        name = @su_object.name
-        name = definition.name if name.length == 0
-        @name = IfcManager::Types::IfcLabel.new(ifc_model, name)
+      @ifc_model = ifc_model
+      @su_object = sketchup
+      definition = @su_object.definition
 
-        # Set IfcProductType
-        if ifc_model.options[:types]
-          if @ifc_model.product_types.key?(definition)
-            @type_product = @ifc_model.product_types[definition]
+      # When instance name is set, use that, otherwise use definition name
+      # (?) set name, here? is this a duplicate?
+      name = @su_object.name
+      name = definition.name if name.length == 0
+      @name = IfcManager::Types::IfcLabel.new(ifc_model, name)
+
+      # Set IfcProductType
+      if ifc_model.options[:types]
+        if @ifc_model.product_types.key?(definition)
+          @type_product = @ifc_model.product_types[definition]
+          @type_product.add_typed_object(self)
+        else
+          type_name = self.class.name.split('::').last + 'Type'
+          if @ifc.const_defined?(type_name)
+            type_product = @ifc.const_get(type_name)
+            @type_product = type_product.new(ifc_model, definition, self.class)
+            @ifc_model.product_types[definition] = @type_product
             @type_product.add_typed_object(self)
-          else
-            type_name = self.class.name.split('::').last + 'Type'
-            if @ifc.const_defined?(type_name)
-              type_product = @ifc.const_get(type_name)
-              @type_product = type_product.new(ifc_model, definition, self.class)
-              @ifc_model.product_types[definition] = @type_product
-              @type_product.add_typed_object(self)
-            end
           end
         end
-        @type_properties = ifc_model.options[:type_properties] && @type_product
-
-        # get attributes from su object and add them to IfcProduct
-        if dicts = definition.attribute_dictionaries
-          dict_reader = BimTools::IfcManager::IfcDictionaryReader.new(ifc_model, self, dicts)
-          dict_reader.set_attributes
-
-          unless @type_properties
-            dict_reader.add_propertysets
-            dict_reader.add_sketchup_definition_properties(ifc_model, self, @su_object.definition)
-            dict_reader.add_classifications
-          end
-          dict_reader.add_sketchup_instance_properties(ifc_model, self, @su_object)
-        end
-
-        # set material if sketchup @su_object has a material
-        # Material added to Product and not to TypeProduct because a Sketchup ComponentDefinition can have a different material for every Instance
-        if ifc_model.options[:materials] && !((is_a? @ifc::IfcFeatureElementSubtraction) || (is_a? @ifc::IfcVirtualElement) || (is_a? @ifc::IfcSpatialStructureElement))
-          material_name = if @su_object.material
-                            @su_object.material.display_name
-                          else
-                            'Default'
-                          end
-
-          # create materialassociation
-          su_material = @su_object.material
-          unless ifc_model.materials.include?(su_material)
-            ifc_model.materials[su_material] = BimTools::IfcManager::MaterialAndStyling.new(ifc_model, su_material)
-          end
-
-          # add product to materialassociation
-          ifc_model.materials[su_material].add_to_material(self)
-        end
-
-        # collect dynamic component attributes if export option is set
-        BimTools::DynamicAttributes.get_dynamic_attributes(ifc_model, self) if ifc_model.options[:dynamic_attributes]
-
       end
+      @type_properties = ifc_model.options[:type_properties] && @type_product
+
+      # get attributes from su object and add them to IfcProduct
+      if dicts = definition.attribute_dictionaries
+        dict_reader = BimTools::IfcManager::IfcDictionaryReader.new(ifc_model, self, dicts)
+        dict_reader.set_attributes
+
+        unless @type_properties
+          dict_reader.add_propertysets
+          dict_reader.add_sketchup_definition_properties(ifc_model, self, @su_object.definition)
+          dict_reader.add_classifications
+        end
+        dict_reader.add_sketchup_instance_properties(ifc_model, self, @su_object)
+      end
+
+      # set material if sketchup @su_object has a material
+      # Material added to Product and not to TypeProduct because a Sketchup ComponentDefinition can have a different material for every Instance
+      if ifc_model.options[:materials] && (is_a? @ifc::IfcElement) && !(is_a? @ifc::IfcFeatureElementSubtraction) && !(is_a? @ifc::IfcVirtualElement)
+
+        # create materialassociation
+        su_material = @su_object.material
+        unless ifc_model.materials.include?(su_material)
+          ifc_model.materials[su_material] = BimTools::IfcManager::MaterialAndStyling.new(ifc_model, su_material)
+        end
+
+        # add product to materialassociation
+        ifc_model.materials[su_material].add_to_material(self)
+      end
+
+      # collect dynamic component attributes if export option is set
+      BimTools::DynamicAttributes.get_dynamic_attributes(ifc_model, self) if ifc_model.options[:dynamic_attributes]
     end
 
     # add export summary for IfcProducts
