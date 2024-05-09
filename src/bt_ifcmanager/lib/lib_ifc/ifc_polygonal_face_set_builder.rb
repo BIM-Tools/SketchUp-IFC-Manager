28f9335b1e69bd8fa841421b76ca557f520ed52c
Jan Brouwer <jbrouwer@digibase.nl>
2024-05-09 09:46:44 +0200

    Update IfcPolygonalFaceSetBuilder header

--- src/bt_ifcmanager/lib/lib_ifc/ifc_polygonal_face_set_builder.rb
+++ src/bt_ifcmanager/lib/lib_ifc/ifc_polygonal_face_set_builder.rb
@@ -2,7 +2,7 @@
 
 #  ifc_polygonal_face_set_builder.rb.rb
 #
-#  Copyright 2022 Jan Brouwer <jan@brewsky.nl>
+#  Copyright 2024 Jan Brouwer <jan@brewsky.nl>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
