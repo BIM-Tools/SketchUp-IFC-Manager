00dace208a4a13d2b013b58f05bfe78ca765c5ac
jbrouwerdigibase <jbrouwer@digibase.nl>
2024-10-13 10:44:10 +0200

    Fix undefined front_texture in IfcTriangulatedFaceSetBuilder

--- src/bt_ifcmanager/lib/lib_ifc/ifc_triangulated_face_set_builder.rb
+++ src/bt_ifcmanager/lib/lib_ifc/ifc_triangulated_face_set_builder.rb
@@ -40,7 +40,7 @@ module BimTools
       end
 
       def initialize(ifc_model)
-        @ifc_module = ifc_model.ifc_module
+        @ifc_module = Settings.ifc_module
         @ifc_model = ifc_model
         @ifc_triangulated_face_set = @ifc_module::IfcTriangulatedFaceSet.new(ifc_model)
         @ifc_triangulated_face_set.coordinates = @ifc_module::IfcCartesianPointList3D.new(ifc_model)
@@ -70,9 +70,9 @@ module BimTools
         add_parent_texture = faces.any? { |face| face.material.nil? }
         add_material_to_model(parent_material) if add_parent_texture
 
-        front_texture = texture_exists?(front_material)
-        back_texture = texture_exists?(back_material) if double_sided_faces
-        parent_texture = texture_exists?(parent_material)
+        front_texture = texture_exists?(front_material) || false
+        back_texture = double_sided_faces ? (texture_exists?(back_material) || false) : false
+        parent_texture = texture_exists?(parent_material) || false
 
         @mirrored = true if is_mirroring?(transformation)
 
@@ -80,9 +80,6 @@ module BimTools
 
         meshes.each do |mesh|
           face_mesh, face_transformation = mesh
-          front_texture = texture_exists?(front_material) || false
-          back_texture = double_sided_faces ? (texture_exists?(back_material) || false) : false
-          parent_texture = texture_exists?(parent_material) || false
 
           # Calculate UV transformation for parent texture if no texture is applied to the face
           if !(front_texture || back_texture) && parent_texture
