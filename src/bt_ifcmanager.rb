409ba1485876b7a8487966cd52af1267679da178
GitHub Action <action@github.com>
2024-12-30 21:55:50 +0000

    Update version to 5.5.0

--- src/bt_ifcmanager.rb
+++ src/bt_ifcmanager.rb
@@ -32,7 +32,7 @@ module BimTools
 
   module IfcManager
     # Version and release information.
-    VERSION = '5.4.0'
+    VERSION = '5.5.0'
 
     # load plugin only if SketchUp version is PRO
     # raised minimum version to 2017 due to switch to htmldialog
