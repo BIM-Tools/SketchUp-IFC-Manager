958a8a77b59c04185a43b4c82690821b1f390c83
jbrouwerdigibase <jbrouwer@digibase.nl>
2025-04-08 15:01:49 +0200

    Create common PropertySets in new default dictionary

--- src/bt_ifcmanager/lib/lib_ifc/add_common_psets.rb
+++ src/bt_ifcmanager/lib/lib_ifc/add_common_psets.rb
@@ -30,86 +30,92 @@ module BimTools
     # @param [String] ent_type_name
     def add_common_psets(definition, ent_type_name)
       ifc_dict = definition.attribute_dictionary(Settings.ifc_version)
-      if ifc_dict
-        entity_type = Settings.ifc_module.const_get(ent_type_name)
-        prefix = Settings.ifc_module
-        entity_type.ancestors.each do |ancestor|
-          name = ancestor.name
-          split_name = ancestor.name.split('::Ifc')
-          next unless split_name.length == 2
+      return unless ifc_dict
 
-          basename = split_name.last
-          next if basename.end_with?('_su')
+      # Ensure the "PropertySets" dictionary exists
+      property_sets_dict = ifc_dict.attribute_dictionary('PropertySets', true)
+      ifc_dict.set_attribute('PropertySets', 'is_hidden', false)
 
-          filename = File.join(PLUGIN_PATH_LIB, 'lib_ifc', 'psd', "Pset_#{basename}Common.xml")
-          xml_file = File.file?(filename)
-          next unless xml_file
+      entity_type = Settings.ifc_module.const_get(ent_type_name)
+      prefix = Settings.ifc_module
+      entity_type.ancestors.each do |ancestor|
+        name = ancestor.name
+        split_name = ancestor.name.split('::Ifc')
+        next unless split_name.length == 2
 
-          xmlfile = File.new(filename)
-          xmldoc = Document.new(xmlfile)
-          xmldoc.elements.each('PropertySetDef') do |e|
-            propertyset_name = e.elements.to_a('Name').first.text
-            pset_dict = ifc_dict.attribute_dictionary(propertyset_name, true)
-            e.elements.each('PropertyDefs/PropertyDef') do |e|
-              property_name = e.elements.to_a('Name').first.text
-              property_type = e.elements.to_a('PropertyType').first.elements.to_a.first.name
+        basename = split_name.last
+        next if basename.end_with?('_su')
 
-              # Only add TypePropertySingleValue and TypePropertyEnumeratedValue
-              # Possible property value types
-              # - TypePropertySingleValue
-              # - TypePropertyEnumeratedValue : "choice"?
-              # - TypePropertyBoundedValue
-              # - TypePropertyReferenceValue
-              if property_type == 'TypePropertySingleValue'
+        filename = File.join(PLUGIN_PATH_LIB, 'lib_ifc', 'psd', "Pset_#{basename}Common.xml")
+        xml_file = File.file?(filename)
+        next unless xml_file
 
-                # Possible sketchup value types
-                # - boolean
-                # - choice
-                # - long # integer?
-                # - double
-                # - string
-                value_type = e.elements.to_a('PropertyType/TypePropertySingleValue/DataType').first.attributes['type']
-                if value_type
-                  attribute_type = if value_type == 'IfcBoolean'
-                                    'boolean'
-                                  # value = false
-                                  elsif value_type == 'IfcInteger'
-                                    'long'
-                                  # value = 0
-                                  elsif value_type.include?('Measure')
-                                    'double'
-                                  # value = 0.0
-                                  else
-                                    'string'
-                                    # value = ""
-                                  end
-                  property_dict = pset_dict.attribute_dictionary(property_name, true)
-                  pset_dict.set_attribute property_name, 'is_hidden', false
-                  value_dict = property_dict.attribute_dictionary(value_type, true)
-                  property_dict.set_attribute value_type, 'attribute_type', attribute_type
-                  property_dict.set_attribute value_type, 'is_hidden', false
-                  property_dict.set_attribute value_type, 'value', nil # value
-                else
-                  puts 'DataType not found'
-                end
-              elsif property_type == 'TypePropertyEnumeratedValue'
-                value_type = e.elements.to_a('PropertyType/TypePropertyEnumeratedValue/EnumList').first.attributes['name']
-                options = e.get_elements('PropertyType/TypePropertyEnumeratedValue/EnumList/EnumItem').map do |e|
-                  e.text
-                end
+        xmlfile = File.new(filename)
+        xmldoc = Document.new(xmlfile)
+        xmldoc.elements.each('PropertySetDef') do |e|
+          propertyset_name = e.elements.to_a('Name').first.text
+
+          # Use the "PropertySets" dictionary as the parent for property sets
+          pset_dict = property_sets_dict.attribute_dictionary(propertyset_name, true)
+          e.elements.each('PropertyDefs/PropertyDef') do |e|
+            property_name = e.elements.to_a('Name').first.text
+            property_type = e.elements.to_a('PropertyType').first.elements.to_a.first.name
+
+            # Only add TypePropertySingleValue and TypePropertyEnumeratedValue
+            # Possible property value types
+            # - TypePropertySingleValue
+            # - TypePropertyEnumeratedValue : "choice"?
+            # - TypePropertyBoundedValue
+            # - TypePropertyReferenceValue
+            if property_type == 'TypePropertySingleValue'
+
+              # Possible sketchup value types
+              # - boolean
+              # - choice
+              # - long # integer?
+              # - double
+              # - string
+              value_type = e.elements.to_a('PropertyType/TypePropertySingleValue/DataType').first.attributes['type']
+              if value_type
+                attribute_type = if value_type == 'IfcBoolean'
+                                   'boolean'
+                                 # value = false
+                                 elsif value_type == 'IfcInteger'
+                                   'long'
+                                 # value = 0
+                                 elsif value_type.include?('Measure')
+                                   'double'
+                                 # value = 0.0
+                                 else
+                                   'string'
+                                   # value = ""
+                                 end
                 property_dict = pset_dict.attribute_dictionary(property_name, true)
                 pset_dict.set_attribute property_name, 'is_hidden', false
                 value_dict = property_dict.attribute_dictionary(value_type, true)
-                property_dict.set_attribute value_type, 'attribute_type', 'enumeration'
+                property_dict.set_attribute value_type, 'attribute_type', attribute_type
                 property_dict.set_attribute value_type, 'is_hidden', false
-                property_dict.set_attribute value_type, 'options', options
-                property_dict.set_attribute value_type, 'value', options.last # value
+                property_dict.set_attribute value_type, 'value', nil # value
+              else
+                puts 'DataType not found'
               end
+            elsif property_type == 'TypePropertyEnumeratedValue'
+              value_type = e.elements.to_a('PropertyType/TypePropertyEnumeratedValue/EnumList').first.attributes['name']
+              options = e.get_elements('PropertyType/TypePropertyEnumeratedValue/EnumList/EnumItem').map do |e|
+                e.text
+              end
+              property_dict = pset_dict.attribute_dictionary(property_name, true)
+              pset_dict.set_attribute property_name, 'is_hidden', false
+              value_dict = property_dict.attribute_dictionary(value_type, true)
+              property_dict.set_attribute value_type, 'attribute_type', 'enumeration'
+              property_dict.set_attribute value_type, 'is_hidden', false
+              property_dict.set_attribute value_type, 'options', options
+              property_dict.set_attribute value_type, 'value', options.last # value
             end
           end
         end
-        false
       end
+      false
     end
   end
 end
