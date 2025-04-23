7a004e2a97b76a9438f6b0b9cb399903587a9915
jbrouwerdigibase <jbrouwer@digibase.nl>
2025-04-23 10:47:55 +0200

    sanitize classification yml data

--- src/bt_ifcmanager/ui/select.rb
+++ src/bt_ifcmanager/ui/select.rb
@@ -27,7 +27,7 @@ module BimTools
     require File.join(PLUGIN_PATH_UI, 'form_element')
     module PropertiesWindow
       class HtmlSelect < FormElement
-        attr_reader:options
+        attr_reader :options
 
         def initialize(dialog, name)
           super(dialog)
@@ -37,40 +37,38 @@ module BimTools
           @index_max = 0
         end
 
-        def set_js_options(options, options_template=[])
-          if options.is_a? Array
-            @options = options
-            @js_options = options_template
-            i = 0
-            while i < options.length() do
-              @js_options << {:id => i, :text => options[i]}
-              i += 1
-            end
-            @index_max = i
+        def set_js_options(options, options_template = [])
+          return unless options.is_a? Array
+
+          @options = options
+          @js_options = options_template
+          i = 0
+          while i < options.length
+            @js_options << { id: i, text: options[i] }
+            i += 1
           end
+          @index_max = i
         end
 
-        def js()
-          if @value
-            if @value == "..."
-              index = "'...'"
-            else
-              index = @options.index(@value)
-            end
-            return @js << "$('##{@id}').val(#{index});\n$('##{@id}').trigger('change');\n"
-          else
-            return @js << "$('##{@id}').val('-');\n$('##{@id}').trigger('change');\n"
-          end
+        def js
+          return @js << "$('##{@id}').val('-');\n$('##{@id}').trigger('change');\n" unless @value
+
+          index = if @value == '...'
+                    "'...'"
+                  else
+                    @options.index(@value)
+                  end
+          @js << "$('##{@id}').val(#{index});\n$('##{@id}').trigger('change');\n"
         end
 
-        def html(selection)
-          if @hidden
-            hide_css = " style=\"display:none;\""
-          else
-            hide_css = ""
-          end
-          if @button == true
-            return <<-HTML
+        def html(_selection)
+          hide_css = if @hidden
+                       ' style="display:none;"'
+                     else
+                       ''
+                     end
+          return <<-HTML if @button == true
+
               <div class="form-group row #{@id}_row"#{hide_css}>
               <label for="#{@id}" class="col-3 col-form-label">#{@name}:</label>
               <div class="input-group col-9 #{@id}_row">
@@ -78,48 +76,47 @@ module BimTools
               <div class="input-group-append">
               <button class="btn" type="button" id="add_#{@id}">+</button>
               </div></div></div>
-            HTML
-          else
-            return <<-HTML
+          HTML
+
+          <<-HTML
               <div class="form-group row #{@id}_row"#{hide_css}>
               <label for="#{@id}" class="col-3 col-form-label">#{@name}:</label>
               <div class="col-9 #{@id}_row">
               <select class="form-control" id="#{@id}" data-width="100%"></select>
               </div></div>
-            HTML
-          end
+          HTML
         end
 
-        def add_button()
+        def add_button
           @button = true
         end
 
-        def set_options(extra=false)
+        def set_options(extra = false)
           json_options = @js_options
-          if extra
-            json = (@js_options + [{:id => extra,:text => extra}]).to_json
-          else
-            json = @js_options.to_json
-          end
+          json = if extra
+                   (@js_options + [{ id: extra, text: extra }]).to_json.force_encoding('UTF-8')
+                 else
+                   @js_options.to_json.force_encoding('UTF-8')
+                 end
           @js = "$('##{@id}').select2({data: #{json}})\n"
           @js << "$('#add_#{@id}').click(function() {sketchup.add_#{@id}()});"
           @onchange = "$('##{@id}').on('select2:select', function (e) { sketchup.#{@id}(e.params.data.text)});"
         end
 
-        def update(selection)
-          set_value()
-          case @value
-          when nil || "-"
-            index = "'-'"
-          when "..."
-            index = "'...'"
-          else
-            index = @options.index(@value)
-          end
-          self.dialog.execute_script("$('##{@id}').val(#{index});\n$('##{@id}').trigger('change');")
+        def update(_selection)
+          set_value
+          index = case @value
+                  when nil || '-'
+                    "'-'"
+                  when '...'
+                    "'...'"
+                  else
+                    @options.index(@value)
+                  end
+          dialog.execute_script("$('##{@id}').val(#{index});\n$('##{@id}').trigger('change');")
         end
 
-        def set_value()
+        def set_value
           puts 'No value update method defined'
         end
 
@@ -128,26 +125,24 @@ module BimTools
         def set_value_from_list(list)
           case list.length
           when 0
-            @value = "-"
+            @value = '-'
           when 1
-            if list[0].nil?
-              @value = "-"
-            else
-              @value = list[0]
-            end
+            @value = if list[0].nil?
+                       '-'
+                     else
+                       list[0]
+                     end
           else
-            self.dialog.execute_script("if(!$('##{@id}').find(\"option[value='...']\").length) {\nvar newOption = new Option('...', '...', false, false);\n$('##{@id}').append(newOption).trigger('change');\n}")
-            @value = "..."
+            dialog.execute_script("if(!$('##{@id}').find(\"option[value='...']\").length) {\nvar newOption = new Option('...', '...', false, false);\n$('##{@id}').append(newOption).trigger('change');\n}")
+            @value = '...'
           end
         end
 
-        def set_callback()
-        end
+        def set_callback; end
 
-        def hide()
-
+        def hide
           # Also hide dropdown when form elements are hidden
-          self.dialog.execute_script("$('##{@id}').select2('close');")
+          dialog.execute_script("$('##{@id}').select2('close');")
           super()
         end
       end
